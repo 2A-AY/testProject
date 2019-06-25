152b9992d89cda60ce039acca9ed1fb37fccc19b
Adam-AY <adam.alaouiti@gmail.com>
2019-06-26 00:27:55 +0100

    Change index com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testProject';
+
+  private originWord: string;
+
+  constructor(private http: HttpClient) {}
+  /**
+   * save_word
+   */
+  public save_word() {
+   console.log(this.originWord);
+  }
 }
